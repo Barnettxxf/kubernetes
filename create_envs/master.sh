36deb345c19818b8df88dfc6713ed90698edbb93
barnett <15102096586@163.com>
2019-05-21 13:12:16 +0000

    master

--- create_envs/master.sh
+++ create_envs/master.sh
@@ -1,1 +1,8 @@
+sudo rm -rf ~/.kube
+sudo swapoff -a
 sudo kubeadm init --kubernetes-version=1.14.1 --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$(hostname -I | awk '{print $1}')
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.11.0/Documentation/kube-flannel.yml
+
