0828cc8c36bc1f23aaf8f4f130633a1ed40efdce
barnettxxf <15102096586@163.com>
2019-05-25 17:23:44 +0800

    glusterfs

---
+++ chapter_03/glusterfs/add-repo.sh
@@ -0,0 +1,3 @@
+sudo apt install software-properties-common -y
+wget -O- https://download.gluster.org/pub/gluster/glusterfs/3.12/rsa.pub | apt-key add -
+sudo add-apt-repository ppa:gluster/glusterfs-3.12
