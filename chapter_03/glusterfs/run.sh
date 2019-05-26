7fc7e2022e93dd3586ce3c9941d1f2dd03e51b2f
barnett <15102096586@163.com>
2019-05-26 13:58:07 +0000

    glusterfs

--- chapter_03/glusterfs/run.sh
+++ chapter_03/glusterfs/run.sh
@@ -1,4 +1,4 @@
 sudo systemctl restart glusterd.service 
 sudo systemctl restart glustereventsd.service
 sudo mkdir -p /glusterfs/distributed
-sudo gluster volume create v01 replica 3 transport tcp gfs01:/glusterfs/distributed gfs02:/glusterfs/distributed gfs03:/glusterfs/distributed
+sudo gluster volume create v01 replica 2 transport tcp gfs01:/glusterfs/distributed gfs02:/glusterfs/distributed force
